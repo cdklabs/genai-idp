fd14755f7788da8d0016334fbcad4228c060ab7c
Rafał Pawłaszek <rpawlasz@amazon.com>
2026-02-24 12:18:27 +0100

    fix(config-resolver): add missing idp_common layer to ConfigurationResolverFunction

--- packages/@cdklabs/genai-idp/src/processing-environment-api/functions/configuration-resolver-function.ts
+++ packages/@cdklabs/genai-idp/src/processing-environment-api/functions/configuration-resolver-function.ts
@@ -6,11 +6,13 @@ SPDX-License-Identifier: Apache-2.0
 import * as path from "path";
 import * as lambda_python from "@aws-cdk/aws-lambda-python-alpha";
 import * as cdk from "aws-cdk-lib";
+import { Stack } from "aws-cdk-lib";
 import * as kms from "aws-cdk-lib/aws-kms";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 import { IConfigurationTable } from "../../configuration-table";
 import { IdpPythonFunctionOptions } from "../../functions/idp-python-function-options";
+import { IdpPythonLayerVersion } from "../../idp-python-layer-version";
 
 /**
  * Properties for configuring the ConfigurationResolverFunction.
@@ -83,6 +85,7 @@ export class ConfigurationResolverFunction
           ].join(" && "),
         ],
       },
+      layers: [IdpPythonLayerVersion.getOrCreate(Stack.of(scope))],
       runtime: lambda.Runtime.PYTHON_3_12,
       timeout: cdk.Duration.seconds(30),
       memorySize: 512,
